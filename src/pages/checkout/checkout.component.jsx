5b48dd1ba3102ba357c9b976d934079244170a2c
RichardArjoon <webdeveloper.rich@gmail.com>
2020-07-18 19:04:07 -0400

    added addItem and removeItem to checkout item component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
 import {
   selectCartItems,
   selectCartTotal,
@@ -28,12 +30,10 @@ const CheckoutPage = ({ cartItems, total }) => (
         <span>Remove</span>
       </div>
     </div>
-    {cartItems.map((cartItem) => (
+    {cartItems.map((cartItem, index) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
-    <div className='total'>
-      <span>Total: $ {total} </span>
-    </div>
+    <div className='total'>TOTAL: ${total}</div>
   </div>
 );
 
